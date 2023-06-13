f2564fd82ecf5509943b19dfecb8df26c024c604
diana ORTEGA <diana.ortega.xebia@ext.pmu.fr>
2023-06-13 22:07:40 +0400

    add ddg search for agent

--- agent_ai.py
+++ agent_ai.py
@@ -7,12 +7,13 @@ from langchain.llms import OpenAI
 
 
 def get_auth():
-    return os.getenv("OPEN_AI_KEY")
+    return os.getenv("OPENAI_API_KEY")
 
 
 def agent_prediction():
+    # you can set for example: model_name="text-davinci-003"
     llm = OpenAI(temperature=0, openai_api_key=get_auth())
-    tools = load_tools(["arxiv", "llm-math"], llm=llm)
+    tools = load_tools(["ddg-search", "llm-math"], llm=llm)
     agent = initialize_agent(
         tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
     print(agent.run(
